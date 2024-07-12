651961014ffd49087b10691900fe110ff5e7b36d
jay-java <jayprajapati.tops@gmail.com>
2024-07-12 19:08:13 +0530

    pattern array

--- CoreJAVA/src/core/P007_Loops.java
+++ CoreJAVA/src/core/P007_Loops.java
@@ -13,29 +13,24 @@ public class P007_Loops {
 		Scanner sc = new Scanner(System.in);
 		System.out.print("enter num to print table : ");
 		int num = sc.nextInt();
-		for(int i=1;i<=10;i++) {
-			System.out.println(num+" X "+i+" = "+(num*i));
+		for (int i = 1; i <= 10; i++) {
+			System.out.println(num + " X " + i + " = " + (num * i));
 		}
-		
-		
-		
-		
-		
-		for (int i = 1; i <= 10; i++) {//entry control
-			System.out.println("for loop : "+i);
+
+		for (int i = 1; i <= 10; i++) {// entry control
+			System.out.println("for loop : " + i);
 		}
 		System.out.println("---------------------");
-		int j=1;
-		while(j<=10) {//entry control
-			System.out.println("while loop : "+j);
+		int j = 1;
+		while (j <= 10) {// entry control
+			System.out.println("while loop : " + j);
 			j++;
 		}
 		System.out.println("---------------------");
-		int k=1;
-		do { //exit control
-			System.out.println("do while : "+k);
+		int k = 1;
+		do { // exit control
+			System.out.println("do while : " + k);
 			k++;
-		}
-		while(k<=10);
+		} while (k <= 10);
 	}
 }
