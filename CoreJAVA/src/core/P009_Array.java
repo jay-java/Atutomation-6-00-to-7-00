0adab35a0a1f2eb0ed4f5ea390c5d29b82268aa4
jay-java <jayprajapati.tops@gmail.com>
2024-07-15 19:27:39 +0530

    array string function

--- CoreJAVA/src/core/P009_Array.java
+++ CoreJAVA/src/core/P009_Array.java
@@ -14,9 +14,29 @@ public class P009_Array {
 		System.out.println("enter size = ");
 		int size = sc.nextInt();
 		int a[] = new int[size];
+		
 		for(int index=0;index<a.length;index++) {
 			System.out.print("enter element at a["+index+"] : ");
 			a[index] = sc.nextInt();
+		}
+		int sum =0;
+		for(int index=0;index<a.length;index++) {
+			 sum=sum+a[index];
+			 System.out.println("sum of elements is  : "+sum);
+		}
+		System.out.println("enter number to search in array : ");
+		int num = sc.nextInt();
+		int counter = 0;
+		for(int index=0;index<a.length;index++) {
+			if(num == a[index]) {
+				counter++;
+			}
+		}
+		if(counter>0) {
+			System.out.println("yes exist "+counter+" times");
+		}
+		else {
+			System.out.println("not exist");
 		}
 	}
 }
